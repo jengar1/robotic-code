a78d7af11f701a208bce06254a064e90348c661c
jengar1 <ebonygarner47@gmail.com>
2025-10-26 01:05:03 -0400

    robotic.java

--- robotic.java
+++ robotic.java
@@ -1,1 +1,6 @@
-
+//speech recognition//
+//processors for visiual and audio data//
+//program multiple languages , AI automation t process different languages//
+//this functionality will enable robot to multitask//
+//think of scanning and weighing packages and then places packages in designated area based of APIs algorthm//
+//integration of various systems //
